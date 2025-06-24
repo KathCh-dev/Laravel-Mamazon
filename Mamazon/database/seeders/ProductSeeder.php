8bb75fbaa56726827019da2a12da5d009935acec
Kathleen <kathleen.chaiffre.pro@gmail.com>
2025-06-24 11:28:17 +0200

    refacto en cours

--- Mamazon/database/seeders/ProductSeeder.php
+++ Mamazon/database/seeders/ProductSeeder.php
@@ -14,37 +14,46 @@ class ProductSeeder extends Seeder
      */
     public function run(): void
     {
+        $categories = Category::all();
+
+        $catMode = $categories[];
+
         $products = [
-            [
-                'product_title' => 'Ordinateur',
-                'product_description' => `C'est un ordinateur qui a certaines capacités dû au matériel qu'il contient. Convient au travail comme au loisir.`,
-                'price' => '800',
-                'id_category' => $category->id_category,
+            'Electronique' => [
+                [
+                    'product_title' => 'Ordinateur',
+                    'product_description' => 'Un ordinateur qui a certaines capacités dû au matériel qu il contient. Convient au travail comme au loisir.',
+                    'price' => '800',
+                ],
             ],
-                        [
-                'product_title' => 'Pull',
-                'product_description' => `C'est un pull. Choisissez la couleur qui vous plaît... ou pas.`,
-                'price' => '39.99',
-                'id_category' => $category->id_category,
+
+            'Vêtements' => [
+                [
+                    'product_title' => 'Pull',
+                    'product_description' => 'Un pull. Choisissez la couleur qui vous plaît... ou pas.',
+                    'price' => '39.99',
+                ],
             ],
-                        [
-                'product_title' => 'Pelle',
-                'product_description' => `C'est une pelle, petite ou non, mais très solide. Vous pouvez vous en servir pour creuser afin d'enterrer ce que (qui) bon vous semble`,
-                'price' => '24.90',
-                'id_category' => $category->id_category,
+
+            'Maison et jardin' => [
+                [
+                    'product_title' => 'Pelle',
+                    'product_description' => 'Une pelle, petite ou non, mais très solide. Vous pouvez vous en servir pour creuser afin d enterrer ce que (qui) bon vous semble',
+                    'price' => '24.90',
+                ],
             ],
         ];
 
         foreach($categories as $category){
-            foreach ($products as $product) {
-            Product::create([
-            'id_product' => \Str::uuid(),
-            'product_title' => $product['product_title'],
-            'slug' => \Str::slug($product['product_title']),
-            'product_description' => $product['product_description'],
-            'price' => $product['price'],
-            'id_category' => $category[$category.$id_category],
-            ]);
+            foreach ($products[$category->name] as $product) {
+                Product::create([
+                    'id_product' => \Str::uuid(),
+                    'product_title' => $product['product_title'],
+                    'slug' => \Str::slug($product['product_title']),
+                    'product_description' => $product['product_description'],
+                    'price' => $product['price'],
+                    'id_category' => $category->category_id,
+                ]);
             }
         }
     }
